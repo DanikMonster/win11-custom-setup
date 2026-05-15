2de6de62cb929c3722eedf93b2fa974bc5309ca9
danikradyukov-collab <danikradyukov@gmail.com>
2026-05-15 21:07:26 +0300

    Added exit option and refined structure

--- script.bat
+++ script.bat
@@ -1,5 +1,5 @@
 @echo off
-:: Change code page to UTF-8 to support Cyrillic characters
+:: Set encoding to UTF-8
 chcp 65001 >nul
 
 :: ======================================================
@@ -22,44 +22,53 @@ echo ======================================================
 echo.
 echo   [1] English
 echo   [2] Русский
+echo   [3] Exit / Выход
 echo.
+echo ======================================================
 set /p lang_choice="> "
 
-if "%lang_choice%"=="1" (
-    set "m_title=WINDOWS 11 CUSTOM SETUP TOOL"
-    set "m_opt0=[0] FULL AUTO BYPASS (Registry + Create User + Restart)"
-    set "m_opt1=[1] Bypass Internet/MSA (BypassNRO)"
-    set "m_opt2=[2] Disable 'Finish setting up' prompts"
-    set "m_opt3=[3] Disable Telemetry and Tracking"
-    set "m_opt4=[4] Create Local Administrator"
-    set "m_opt5=[5] Restart Computer"
-    set "m_opt6=[6] Exit"
-    set "m_select=Select an option (0-6): "
-    set "m_working=WORKING..."
-    set "m_done=Done! Press any key to return to menu."
-    set "m_restart_msg=The computer will restart in 5 seconds..."
-    set "m_user_name=Enter NEW username: "
-    set "m_user_pass=Enter password (leave blank for none): "
-    set "m_user_done=User created and added to Administrators."
-    set "m_super_info=Applying registry tweaks and creating user..."
-) else (
-    set "m_title=WINDOWS 11 CUSTOM SETUP TOOL"
-    set "m_opt0=[0] ПОЛНЫЙ АВТО-ОБХОД (Реестр + Создание Юзера + Рестарт)"
-    set "m_opt1=[1] Обход интернета и аккаунта Microsoft (BypassNRO)"
-    set "m_opt2=[2] Отключить окно 'Завершение настройки устройства'"
-    set "m_opt3=[3] Отключить базовую телеметрию и слежку"
-    set "m_opt4=[4] Создать локального администратора"
-    set "m_opt5=[5] Перезагрузить компьютер"
-    set "m_opt6=[6] Выход"
-    set "m_select=Выберите пункт (0-6): "
-    set "m_working=ВЫПОЛНЯЕТСЯ..."
-    set "m_done=Готово! Нажмите любую клавишу для возврата в меню."
-    set "m_restart_msg=Компьютер будет перезагружен через 5 секунд..."
-    set "m_user_name=Введите имя НОВОГО пользователя: "
-    set "m_user_pass=Введите пароль (или оставьте пустым): "
-    set "m_user_done=Пользователь создан и добавлен в группу Администраторы."
-    set "m_super_info=Применяем твики реестра и создаем пользователя..."
-)
+if "%lang_choice%"=="1" goto set_en
+if "%lang_choice%"=="2" goto set_ru
+if "%lang_choice%"=="3" exit
+goto lang_select
+
+:set_en
+set "m_title=WINDOWS 11 CUSTOM SETUP TOOL"
+set "m_opt0=[0] FULL AUTO BYPASS (Registry + Create User + Restart)"
+set "m_opt1=[1] Bypass Internet/MSA (BypassNRO)"
+set "m_opt2=[2] Disable 'Finish setting up' prompts"
+set "m_opt3=[3] Disable Telemetry and Tracking"
+set "m_opt4=[4] Create Local Administrator"
+set "m_opt5=[5] Restart Computer"
+set "m_opt6=[6] Exit to Language Selection"
+set "m_select=Select an option (0-6): "
+set "m_working=WORKING..."
+set "m_done=Done! Press any key to return to menu."
+set "m_restart_msg=The computer will restart in 5 seconds..."
+set "m_user_name=Enter NEW username: "
+set "m_user_pass=Enter password (leave blank for none): "
+set "m_user_done=User created and added to Administrators."
+set "m_super_info=Applying registry tweaks and creating user..."
+goto menu
+
+:set_ru
+set "m_title=WINDOWS 11 CUSTOM SETUP TOOL"
+set "m_opt0=[0] ПОЛНЫЙ АВТО-ОБХОД (Реестр + Создание Юзера + Рестарт)"
+set "m_opt1=[1] Обход интернета и аккаунта Microsoft (BypassNRO)"
+set "m_opt2=[2] Отключить окно 'Завершение настройки устройства'"
+set "m_opt3=[3] Отключить базовую телеметрию и слежку"
+set "m_opt4=[4] Создать локального администратора"
+set "m_opt5=[5] Перезагрузить компьютер"
+set "m_opt6=[6] Вернуться к выбору языка"
+set "m_select=Выберите пункт (0-6): "
+set "m_working=ВЫПОЛНЯЕТСЯ..."
+set "m_done=Готово! Нажмите любую клавишу для возврата в меню."
+set "m_restart_msg=Компьютер будет перезагружен через 5 секунд..."
+set "m_user_name=Введите имя НОВОГО пользователя: "
+set "m_user_pass=Введите пароль (или оставьте пустым): "
+set "m_user_done=Пользователь создан и добавлен в группу Администраторы."
+set "m_super_info=Применяем твики реестра и создаем пользователя..."
+goto menu
 
 :menu
 cls
@@ -85,7 +94,7 @@ if "%choice%"=="2" goto scoobe
 if "%choice%"=="3" goto telemetry
 if "%choice%"=="4" goto localuser
 if "%choice%"=="5" goto restart
-if "%choice%"=="6" exit
+if "%choice%"=="6" goto lang_select
 goto menu
 
 :super_bypass
